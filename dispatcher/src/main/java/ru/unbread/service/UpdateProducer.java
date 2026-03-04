7769b5aef80cef47ccb027618f1be9c24dba544d
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2026-03-04 14:20:09 +0300

    v1

---
+++ dispatcher/src/main/java/ru/unbread/service/UpdateProducer.java
@@ -0,0 +1,7 @@
+package ru.unbread.service;
+
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public interface UpdateProducer {
+    void produce(String rabbitQueue, Update update);
+}
---
+++ dispatcher/src/main/java/ru/unbread/service/impl/UpdateProductImpl.java
@@ -0,0 +1,23 @@
+package ru.unbread.service.impl;
+
+import lombok.extern.log4j.Log4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import ru.unbread.service.UpdateProducer;
+
+@Service
+@Log4j
+public class UpdateProductImpl implements UpdateProducer {
+    private final RabbitTemplate rabbitTemplate;
+
+    public UpdateProductImpl(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Override
+    public void produce(String rabbitQueue, Update update) {
+        log.debug(update.getMessage().getText());
+        rabbitTemplate.convertAndSend(rabbitQueue, update);
+    }
+}
